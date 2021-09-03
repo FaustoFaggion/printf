de4eb26e03049e2105b418a4a262775bbfa5522e
Fausto Faggion <faustofaggion@hotmail.com>
2021-09-03 16:12:49 -0300

    finish mandatory

--- ft_printf.c
+++ ft_printf.c
@@ -11,7 +11,10 @@ static int	type(const char *c, va_list ptr)
 	else if (*c == 's')
 		len = wr_str(ptr);
 	else if (*c == 'p')
-		len = wr_p_hex(ptr);
+	{
+		len = wr_hex(ptr, *c);
+		//len = wr_p_hex(ptr);
+	}
 	else if (*c == 'd')
 		len = wr_int(ptr);
 	else if (*c == 'i')
